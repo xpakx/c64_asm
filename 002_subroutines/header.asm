9bf6c320099f55ae2ce61a33a24517a1366b5c32
xpakx <55586470+xpakx@users.noreply.github.com>
2026-05-13 23:00:03 +0200

    chore: add constants for c64 colors

--- 002_subroutines/header.asm
+++ 002_subroutines/header.asm
@@ -8,7 +8,24 @@ SCREEN_RAM = $0400
 COLOR_RAM = $D800
 CUR_COLOR = $0286
 
-WHITE = $01     
-
 STR_PTR = $FB      
 
+; ---------
+; Colors
+; ---------
+BLACK = $00
+WHITE = $01     
+RED = $02
+CYAN = $03
+PURPLE = $04
+GREEN = $05
+BLUE = $06
+YELLOW = $07
+ORANGE = $08
+BROWN = $09
+LIGHT_RED = $0A
+GREY_1 = $0B
+GREY_2 = $0C
+LIGHT_GREEN = $0D
+LIGHT_BLUE = $0E
+GREY_3 = $0F
