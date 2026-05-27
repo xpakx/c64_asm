d30ec8568d9f927a52ee108aa30363e3bb8c6326
xpakx <55586470+xpakx@users.noreply.github.com>
2026-05-27 22:35:14 +0200

    chore(sprites): declare constants necessary for sprites

---
+++ 007_sprites/main.asm
@@ -0,0 +1,37 @@
+; ------------
+; Commodore 64 ASM
+; ------------
+
+.include 'header.asm'
+.include 'basic.asm'
+
+*= $0810 
+
+start:
+	lda #RED
+	sta BORDER_COLOR
+	lda #BLACK
+	sta BG_COLOR
+
+program_loop:
+
+get_key:
+        jsr GETIN
+        beq program_loop
+        
+        cmp #Q_KEY
+        beq exit_prog
+
+	jmp program_loop
+
+exit_prog:
+	jsr clr_scr
+	lda #WHITE
+	sta CUR_COLOR  
+	lda #LIGHT_BLUE
+	sta BORDER_COLOR
+	lda #BLUE
+	sta BG_COLOR
+        rts
+
+.include 'subroutines.asm'
