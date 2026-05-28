131966685f6a8850fea33b38a1341cb325bb1138
xpakx <55586470+xpakx@users.noreply.github.com>
2026-05-28 22:13:18 +0200

    feat(spriteeditor): create window for editor and import raylib

--- utils/sprite_editor/main.c
+++ utils/sprite_editor/main.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main() {
-   printf("C64 sprite editor");
+   const int screenWidth = 800;
+   const int screenHeight = 450;
+
+   InitWindow(screenWidth, screenHeight, "C64 Sprite Editor");
+
+   SetTargetFPS(60);
+
+   while (!WindowShouldClose())
+   {
+	   BeginDrawing();
+
+	   ClearBackground(RAYWHITE);
+
+	   DrawText("C64 Sprite Editor", 350, 200, 20, DARKGRAY);
+
+	   EndDrawing();
+   }
+
+   CloseWindow();
    return 0;
 }
 
