99e70e59b05d3f866413b9989fa3fb4dd0b3c0fe
xpakx <55586470+xpakx@users.noreply.github.com>
2026-06-04 20:23:32 +0200

    chore(interrupts): init interrupts example

---
+++ 010_interrupts/main.asm
@@ -0,0 +1,45 @@
+; ------------
+; Commodore 64 ASM
+; ------------
+
+.include 'header.asm'
+SOURCE_LOW = $20
+SOURCE_HIGH = $21
+
+DEST_LOW = $24
+DEST_HIGH = $25
+
+.include 'basic.asm'
+
+*= $0810 
+
+start:
+	jsr clr_scr
+	lda #RED
+	sta BORDER_COLOR
+	lda #BLACK
+	sta BG_COLOR
+
+
+program_loop:
+
+get_key:
+        jsr GETIN
+        beq program_loop
+        
+        cmp #Q_KEY
+        beq exit_prog
+
+	jmp program_loop
+
+exit_prog:
+	jsr clr_scr
+	lda #WHITE
+	sta CUR_COLOR  
+	lda #LIGHT_BLUE
+	sta BORDER_COLOR
+	lda #BLUE
+	sta BG_COLOR
+        rts
+
+.include 'subroutines.asm'
