5308f44d8b60ae944edea13873a9e81ba9ee2e73
xpakx <55586470+xpakx@users.noreply.github.com>
2026-06-07 23:36:16 +0200

    feat(snake2): add debug flag

---
+++ 012_snake2/interrupts.asm
@@ -0,0 +1,75 @@
+init_interrupts:
+    sei
+    lda #%01111111
+    sta CIA1_ICR
+
+    and $D011
+    sta $D011
+
+    lda CIA1_ICR
+    lda CIA2_ICR
+    
+    lda #242
+    sta VIC_RASTER
+
+    lda VIC_CONTROL_1
+    and #%01111111
+    sta VIC_CONTROL_1
+
+    lda #<irq
+    sta IRR_ADDR_LOW
+    lda #>irq
+    sta IRR_ADDR_HIGH
+
+    lda #%00000001
+    sta VIC_IRM
+    
+    cli
+    rts
+
+clean_interrupts:
+    sei
+    
+    lda #%00000000
+    sta VIC_IRM
+    
+    lda #%10000001
+    sta CIA1_ICR
+    
+    lda #$31
+    sta IRR_ADDR_LOW
+    lda #$EA
+    sta IRR_ADDR_HIGH
+
+    asl VIC_IRR
+    
+    cli
+    rts
+
+irq:
+    dec TIMER_TICK
+    bne exit_irq
+
+    lda #15
+    sta TIMER_TICK
+
+    dec COUNTER
+    lda COUNTER
+
+    bne update_digit
+    lda #9
+    sta COUNTER
+update_digit:
+.if DEBUG
+    clc
+    adc #$30
+    sta SCREEN
+.endif
+
+logic:
+    lda #%10000000
+    sta FRAME_FLAG
+
+exit_irq:
+    asl VIC_IRR
+    jmp $EA31
--- 012_snake2/main.asm
+++ 012_snake2/main.asm
@@ -14,6 +14,8 @@ PLAYER_ROW = $22
 PLAYER_COL = $23
 FRAME_FLAG = $24
 
+DEBUG = 1
+
 .include 'basic.asm'
 
 *= $0810 
@@ -25,11 +27,13 @@ start:
     lda #BLACK
     sta BG_COLOR
 
+.if DEBUG
     lda #9
     sta COUNTER
     clc
     adc #$30
     sta SCREEN
+.endif
 
     lda #15
     sta TIMER_TICK
@@ -105,80 +109,6 @@ exit_prog:
     sta BG_COLOR
     rts
 
-init_interrupts:
-    sei
-    lda #%01111111
-    sta CIA1_ICR
-
-    and $D011
-    sta $D011
-
-    lda CIA1_ICR
-    lda CIA2_ICR
-    
-    lda #242
-    sta VIC_RASTER
-
-    lda VIC_CONTROL_1
-    and #%01111111
-    sta VIC_CONTROL_1
-
-    lda #<irq
-    sta IRR_ADDR_LOW
-    lda #>irq
-    sta IRR_ADDR_HIGH
-
-    lda #%00000001
-    sta VIC_IRM
-    
-    cli
-    rts
-
-clean_interrupts:
-    sei
-    
-    lda #%00000000
-    sta VIC_IRM
-    
-    lda #%10000001
-    sta CIA1_ICR
-    
-    lda #$31
-    sta IRR_ADDR_LOW
-    lda #$EA
-    sta IRR_ADDR_HIGH
-
-    asl VIC_IRR
-    
-    cli
-    rts
-
-irq:
-    dec TIMER_TICK
-    bne exit_irq
-
-    lda #15
-    sta TIMER_TICK
-
-    dec COUNTER
-    lda COUNTER
-
-    bne update_digit
-    lda #9
-    sta COUNTER
-update_digit:
-    clc
-    adc #$30
-    sta SCREEN
-
-logic:
-    lda #%10000000
-    sta FRAME_FLAG
-
-exit_irq:
-    asl VIC_IRR
-    jmp $EA31
-
 
 init_player:
     lda #$0A
@@ -228,6 +158,7 @@ get_rand:
 
 .include 'movement.asm'
 .include 'subroutines.asm'
+.include 'interrupts.asm'
 
 seed:
 	.byte $F5
