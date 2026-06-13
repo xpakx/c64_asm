1174a3231f930d5a287f1851a6924339b8c71204
xpakx <55586470+xpakx@users.noreply.github.com>
2026-06-13 22:31:21 +0200

    feat(sid): add more notes and make simple toy program for music

--- 014_sid/main.asm
+++ 014_sid/main.asm
@@ -44,7 +44,61 @@ get_key:
 
 	cmp #KEY_1
 	beq play_c
+	cmp #KEY_2
+	beq play_d
+	cmp #KEY_3
+	beq play_e
+	cmp #KEY_4
+	beq play_f
+	cmp #KEY_5
+	beq play_g
+	cmp #KEY_6
+	beq play_a
+	cmp #KEY_7
+	beq play_h
 
+	jmp program_loop
+
+play_c:
+	ldx #$65
+	ldy #$11
+	jsr play_note
+	jmp program_loop
+
+play_d:
+	ldx #$88
+	ldy #$13
+	jsr play_note
+	jmp program_loop
+
+play_e:
+	ldx #$ED
+	ldy #$15
+	jsr play_note
+	jmp program_loop
+
+play_f:
+	ldx #$3B
+	ldy #$17
+	jsr play_note
+	jmp program_loop
+
+play_g:
+	ldx #$13
+	ldy #$1A
+	jsr play_note
+	jmp program_loop
+
+play_a:
+	ldx #$45
+	ldy #$1D
+	jsr play_note
+	jmp program_loop
+
+play_h:
+	ldx #$DA
+	ldy #$20
+	jsr play_note
 	jmp program_loop
 
 exit_prog:
@@ -59,18 +113,18 @@ exit_prog:
 	sta BG_COLOR
         rts
 
-play_c:
+play_note
 	lda #$20
 	sta V1_CTRL
 
-	lda #$65            
+	txa    
 	sta V1_FREQ_LO
-	lda #$11            
+	tya
 	sta V1_FREQ_HI
 
 	lda #$21
 	sta V1_CTRL
 
-	jmp program_loop
+	rts
 
 .include 'subroutines.asm'
